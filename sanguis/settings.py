308938bec40a7eebc0102e68c27a79391bedf177
Arijit065 <Arijit065@users.noreply.github.com>
2026-04-30 22:17:49 +0530

    refactor: Rename config package from bloodbankmanagement to sanguis

--- sanguis/settings.py
+++ sanguis/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CSRF_COOKIE_SECURE=False
-ROOT_URLCONF = 'bloodbankmanagement.urls'
+ROOT_URLCONF = 'sanguis.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'bloodbankmanagement.wsgi.application'
+WSGI_APPLICATION = 'sanguis.wsgi.application'
 
 
 # Database
